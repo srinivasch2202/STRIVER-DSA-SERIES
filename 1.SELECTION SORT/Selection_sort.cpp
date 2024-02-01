b456faf361ec7af6244ce9be5fea12bb99a21521
enter <enter@enter.enter>
2024-02-01 22:10:39 +0530

    selection sort

---
+++ 1.SELECTION SORT/Selection_sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print(vector<int> arr){
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void selection_sort(vector<int> arr){
+    for(int i=0;i<arr.size()-1;i++){
+        int min=i;
+        for(int j=i+1;j<arr.size();j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        swap(arr[i],arr[min]);
+
+    }
+    print(arr);
+}
+int main(){
+    vector<int> arr{13,46,24,52,20,9};
+    selection_sort(arr);    
+}
